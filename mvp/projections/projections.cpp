8278802a78bdaf8aed9817ec45eb34cfbb918818
kofes <turnever@gmail.com>
2018-02-08 22:59:39 +1000

    fix bug with return-type

--- mvp/projections/projections.cpp
+++ mvp/projections/projections.cpp
@@ -13,7 +13,7 @@ linear_algebra::Matrix ortho(
 ) {
     if (src.width() != src.height() || src.width() != 4)
         return linear_algebra::Matrix();
-    return {
+    return linear_algebra::Matrix {
         {2 / (right - left),         0         ,         0        , - (right + left) / (right - left)},
         {        0         , 2 / (top - bottom),         0        , - (top + bottom) / (top - bottom)},
         {        0         ,         0         , -2 / (far - near),   - (far + near) / (far - near)  },
@@ -32,7 +32,7 @@ linear_algebra::Matrix perspective(
 ) {
     if (src.width() != src.height() || src.width() != 4)
         return linear_algebra::Matrix();
-    return {
+    return linear_algebra::Matrix {
                    {2 * near / (right - left),            0             , (right + left) / (right - left),                0               },
                    {            0            , 2 * near / (top - bottom), (top + bottom) / (top - bottom),                0               },
                    {            0            ,            0             , - (far + near) / (far - near)  , - 2 * far * near / (far - near)},
