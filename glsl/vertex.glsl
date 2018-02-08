c39d56c5e5f57d779c5be3ba99e784d23bc1aa0e
kofes <turnever@gmail.com>
2018-02-09 01:56:09 +1000

    rename out params

--- glsl/vertex.glsl
+++ glsl/vertex.glsl
@@ -11,33 +11,37 @@ in vec2 obj_texture;
 in vec3 light_position;
 in vec3 camera_position;
 
-out vec3 normal_light;
-out vec3 normal_camera;
-out vec3 normal;
-out vec2 texture_obj;
-out float distance_light;
+out vec3 camera_normal;
+
+out vec3 light_normal;
+out float light_distance;
+
+out vec3 material_normal;
+
+out vec2 texture_uv;
 
 void main() {
     gl_Position = mvp * vec4(obj_position, 1.0);
 
     vec3 model_position = mat3(model) * obj_position;
 
-    distance_light = distance(light_position, model_position);
-    normal_light = normalize(light_position - model_position);
-    normal_camera = normalize(camera_position - model_position);
+    light_normal = normalize(light_position - model_position);
+    light_distance = distance(light_position, model_position);
 
-    texture_obj = obj_texture;
+    camera_normal = normalize(camera_position - model_position);
 
-    normal = mat3(model) * normalize(obj_normal);
+    texture_uv = obj_texture;
+
+    material_normal = mat3(model) * normalize(obj_normal);
     vec3 tangent = mat3(model) * normalize(obj_tangent);
     vec3 bitangent = mat3(model) * normalize(obj_bitangent);
 
     mat3 TBN = transpose(mat3(
         tangent,
         bitangent,
-        normal
+        material_normal
     ));
 
-    normal_light = TBN * normal_light;
-    normal_camera = TBN * normal_camera;
+    light_normal = TBN * light_normal;
+    camera_normal = TBN * camera_normal;
 }
