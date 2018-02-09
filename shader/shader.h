b20cd5a2b1773f0305b3d5c485cd6e99ceb5a061
kofes <turnever@gmail.com>
2018-02-09 10:02:32 +1000

    add logger to shader class

--- shader/shader.h
+++ shader/shader.h
@@ -7,16 +7,25 @@
 
 #include "shader_error.h"
 
+//#define SHADER_LOG
+
+#ifdef SHADER_LOG
+#include <iostream>
+#define SHADER_LOGout (std::cout << "SHADER PROGRAM: ")
+#else
+#define SHADER_LOGout (std::ostream(0))
+#endif
+
 namespace glsl {
 namespace shader {
 class shader {
 public:
     explicit shader(GLenum type);
-    explicit shader(GLenum type, std::ifstream& src);
+    explicit shader(GLenum type, std::ifstream&& src);
     explicit shader(GLenum type, const std::string& src);
     ~shader();
 
-    shader& exec(std::ifstream& src);
+    shader& exec(std::ifstream&& src);
     shader& exec(const std::string& src);
 
     shader& compile();
