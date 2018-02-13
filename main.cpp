ae10cf92451a3113735d6b54966aa408c418f256
kofes <turnever@gmail.com>
2018-02-13 22:16:24 +1000

    draw grid at main with debug

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <math_model/Orbit.h>
 #include "GL/glew.h"
 #include "GL/freeglut.h"
 
@@ -13,14 +14,32 @@
 #include "geometric_object/grid.h"
 
 #include "mvp/camera/camera.h"
-#include <memory>
 
 #define MAIN_LOG
+#define MAIN_DBG
 
 #ifdef MAIN_LOG
 #define LOGout (std::cout << "Main log: ")
 #else
 #define LOGout (std::ostream(0))
+#endif
+
+#ifdef MAIN_DBG
+#define DBGout (std::cout << "Main debug: ")
+void MAIN_DBG_MessageCallback(
+        GLenum source,
+        GLenum type,
+        GLuint id,
+        GLenum severity,
+        GLsizei length,
+        const GLchar* message,
+        const void* userParam
+) {
+    fprintf(stderr, "OpenGL DEBUG[%s]:\n\ttype = 0x%x,\n\tseverity = 0x%x,\n\tmessage = %s\n",
+             (type == GL_DEBUG_TYPE_ERROR ? "ERROR" : "LOG"), type, severity, message);
+}
+#else
+#define DBGout (std::ostream(0))
 #endif
 
 using namespace std;
@@ -41,36 +60,29 @@ static mvp::Camera camera;
 static std::map<std::string, GLuint> shmap;
 static std::map<int, bool> keymap;
 
-// DEBUG
-static linear_algebra::Matrix prev_camera;
-
-int main(int argc, char* argv[]) {
+int init(int argc, char* argv[]) {
     serialize::args(argc, argv)
-    .handle("argument", [&] (const serialize::values& values, const string& error) {
-        for (const string& value: values)
-            cout << value << endl;
-    })
-    .handle("width", [&] (const serialize::values& values, const string& error) {
-        int tmp = 0;
-        for (const string& value: values) {
-            stringstream sstream(value);
-            sstream >> tmp;
-            if (tmp > 0) break;
-        }
-        if (tmp > 0) width = tmp;
-    })
-    .handle("height", [&] (const serialize::values& values, const string& error) {
-        int tmp = 0;
-        for (const string& value: values) {
-            stringstream sstream(value);
-            sstream >> tmp;
-            if (tmp > 0) break;
-        }
-        if (tmp > 0) height = tmp;
-    })
-    .handle("title", [&] (const serialize::values& values, const string& error) {
-        title = values.empty() ? title : values.front();
-    });
+            .handle("width", [&] (const serialize::values& values, const string& error) {
+                int tmp = 0;
+                for (const string& value: values) {
+                    stringstream sstream(value);
+                    sstream >> tmp;
+                    if (tmp > 0) break;
+                }
+                if (tmp > 0) width = tmp;
+            })
+            .handle("height", [&] (const serialize::values& values, const string& error) {
+                int tmp = 0;
+                for (const string& value: values) {
+                    stringstream sstream(value);
+                    sstream >> tmp;
+                    if (tmp > 0) break;
+                }
+                if (tmp > 0) height = tmp;
+            })
+            .handle("title", [&] (const serialize::values& values, const string& error) {
+                title = values.empty() ? title : values.front();
+            });
 
     // GLUT initialization
     glutInit(&argc, argv);
@@ -88,62 +100,71 @@ int main(int argc, char* argv[]) {
     glewExperimental = GL_TRUE;
     GLenum error_code = glewInit();
     if (error_code != GLEW_OK) {
-        cout << "Error: " << glewGetErrorString(error_code) << endl;
+        LOGout << "Error: " << glewGetErrorString(error_code) << endl;
         return 1;
     }
-    cout << "Status: using GLEW " << glewGetString(GLEW_VERSION) << endl;
+
+    LOGout << "Status: using GLEW " << glewGetString(GLEW_VERSION) << endl;
+
+#ifdef MAIN_DBG
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback((GLDEBUGPROC)MAIN_DBG_MessageCallback, nullptr);
+#endif
 
     glClearColor(0.f, 0.f, 0.f, 0.f);
     glViewport(0, 0, width, height);
 
-    camera = mvp::Camera(75, width/height, 0.5, 100);
-
-    prev_camera = camera.model();
+    camera = mvp::Camera(75, width/(float)height, 0.5, 100);
 
     try {
         glsl::shader::program program;
         program_id = program
-            .add(glsl::shader::shader(GL_VERTEX_SHADER, ifstream("glsl/vertex.glsl")).compile())
-            .add(glsl::shader::shader(GL_FRAGMENT_SHADER, ifstream("glsl/fragment.glsl")).compile())
-            .link()
-            // vertex shader
-            ("mvp", glsl::shader::field::uniform, [&](GLint id) {shmap["mvp"] = id;})
-            ("model", glsl::shader::field::uniform, [&](GLint id) {shmap["model"] = id;})
-            ("camera_position", glsl::shader::field::attribute, [&](GLint id) {shmap["camera_position"] = id;})
-            ("light_position", glsl::shader::field::attribute, [&](GLint id) {shmap["light_position"] = id;})
-            ("obj_position", glsl::shader::field::attribute, [&](GLint id) {shmap["obj_position"] = id;})
-            ("obj_normal", glsl::shader::field::attribute, [&](GLint id) {shmap["obj_normal"] = id;})
-            ("obj_tangent", glsl::shader::field::attribute, [&](GLint id) {shmap["obj_tangent"] = id;})
-            ("obj_bitangent", glsl::shader::field::attribute, [&](GLint id) {shmap["obj_bitangent"] = id;})
-            ("obj_texture", glsl::shader::field::attribute, [&](GLint id) {shmap["obj_texture"] = id;})
-            // fragment shader
-            ("light_ambient", glsl::shader::field::uniform, [&](GLint id) {shmap["light_ambient"] = id;})
-            ("light_diffuse", glsl::shader::field::uniform, [&](GLint id) {shmap["light_diffuse"] = id;})
-            ("light_specular", glsl::shader::field::uniform, [&](GLint id) {shmap["light_specular"] = id;})
-            ("light_emission", glsl::shader::field::uniform, [&](GLint id) {shmap["light_emission"] = id;})
-            ("attenuation_constant", glsl::shader::field::uniform, [&](GLint id) {shmap["attenuation_constant"] = id;})
-            ("attenuation_linear", glsl::shader::field::uniform, [&](GLint id) {shmap["attenuation_linear"] = id;})
-            ("attenuation_quadratic", glsl::shader::field::uniform, [&](GLint id) {shmap["attenuation_quadratic"] = id;})
-            ("spot_normal", glsl::shader::field::uniform, [&](GLint id) {shmap["spot_normal"] = id;})
-            ("spot_cutoff", glsl::shader::field::uniform, [&](GLint id) {shmap["spot_cutoff"] = id;})
-            ("spot_exponent", glsl::shader::field::uniform, [&](GLint id) {shmap["spot_exponent"] = id;})
-            ("material_ambient", glsl::shader::field::uniform, [&](GLint id) {shmap["material_ambient"] = id;})
-            ("material_diffuse", glsl::shader::field::uniform, [&](GLint id) {shmap["material_diffuse"] = id;})
-            ("material_specular", glsl::shader::field::uniform, [&](GLint id) {shmap["material_specular"] = id;})
-            ("material_emission", glsl::shader::field::uniform, [&](GLint id) {shmap["material_emission"] = id;})
-            ("material_shininess", glsl::shader::field::uniform, [&](GLint id) {shmap["material_shininess"] = id;})
-            ("normal_sampler", glsl::shader::field::uniform, [&](GLint id) {shmap["normal_sampler"] = id;})
-            ("diffuse_sampler", glsl::shader::field::uniform, [&](GLint id) {shmap["diffuse_sampler"] = id;})
-            ("specular_sampler", glsl::shader::field::uniform, [&](GLint id) {shmap["specular_sampler"] = id;})
-            ("select_samplers", glsl::shader::field::uniform, [&](GLint id) {shmap["select_samplers"] = id;})
-            .id();
+            // 'make' shader program
+                .add(glsl::shader::shader(GL_VERTEX_SHADER, ifstream("glsl/vertex.glsl")).compile())
+                .add(glsl::shader::shader(GL_FRAGMENT_SHADER, ifstream("glsl/fragment.glsl")).compile())
+                .link()
+            // [vertex shader]: link attributes & uniforms
+                .link("mvp", glsl::shader::field::uniform, shmap["mvp"])
+                .link("model", glsl::shader::field::uniform, shmap["model"])
+                .link("camera_position", glsl::shader::field::attribute, shmap["camera_position"])
+                .link("light_position", glsl::shader::field::attribute, shmap["light_position"])
+                .link("obj_position", glsl::shader::field::attribute, shmap["obj_position"])
+                .link("obj_normal", glsl::shader::field::attribute, shmap["obj_normal"])
+                .link("obj_tangent", glsl::shader::field::attribute, shmap["obj_tangent"])
+                .link("obj_bitangent", glsl::shader::field::attribute, shmap["obj_bitangent"])
+                .link("obj_texture", glsl::shader::field::attribute, shmap["obj_texture"])
+            // [fragment shader]: link attributes & uniforms
+                .link("light_diffuse", glsl::shader::field::uniform, shmap["light_diffuse"])
+                .link("light_specular", glsl::shader::field::uniform, shmap["light_specular"])
+                .link("attenuation_constant", glsl::shader::field::uniform, shmap["attenuation_constant"])
+                .link("attenuation_linear", glsl::shader::field::uniform, shmap["attenuation_linear"])
+                .link("attenuation_quadratic", glsl::shader::field::uniform, shmap["attenuation_quadratic"])
+                .link("spot_normal", glsl::shader::field::uniform, shmap["spot_normal"])
+                .link("spot_cutoff", glsl::shader::field::uniform, shmap["spot_cutoff"])
+                .link("spot_exponent", glsl::shader::field::uniform, shmap["spot_exponent"])
+                .link("material_ambient", glsl::shader::field::uniform, shmap["material_ambient"])
+                .link("material_diffuse", glsl::shader::field::uniform, shmap["material_diffuse"])
+                .link("material_specular", glsl::shader::field::uniform, shmap["material_specular"])
+                .link("material_emission", glsl::shader::field::uniform, shmap["material_emission"])
+                .link("material_shininess", glsl::shader::field::uniform, shmap["material_shininess"])
+                .link("select_samplers", glsl::shader::field::uniform, shmap["select_samplers"])
+                .link("normal_sampler", glsl::shader::field::uniform, shmap["normal_sampler"])
+                .link("diffuse_sampler", glsl::shader::field::uniform, shmap["diffuse_sampler"])
+                .link("specular_sampler", glsl::shader::field::uniform, shmap["specular_sampler"])
+            // return id of shader program
+                .id();
     } catch (std::exception& e) {
-        cout << "Exception: " << e.what() << endl;
+        LOGout << "Exception: " << e.what() << endl;
         return 1;
     }
 
-    // Bind engine functions
+    return 0;
+}
 
+int renderer(int argc, char* argv[]) {
+    init(argc, argv);
+
+    // Bind engine functions
     glutKeyboardFunc(keyboard_handler);
     glutKeyboardUpFunc(keyboard_up_handler);
 
@@ -164,6 +185,8 @@ void keyboard_handler(unsigned char button, int x, int y) {
         case 's': keymap['s'] = true; break;
         case 'd': keymap['d'] = true; break;
         case 'a': keymap['a'] = true; break;
+        case ' ': keymap[' '] = true; break;
+        case 'c': keymap['c'] = true; break;
         default: break;
     }
 }
@@ -173,6 +196,8 @@ void keyboard_up_handler(unsigned char button, int x, int y) {
         case 's': keymap['s'] = false; break;
         case 'd': keymap['d'] = false; break;
         case 'a': keymap['a'] = false; break;
+        case ' ': keymap[' '] = false; break;
+        case 'c': keymap['c'] = false; break;
         default: break;
     }
 }
@@ -187,24 +212,25 @@ void mouse_handler(int button, int state, int x, int y) {
 }
 
 void mouse_motion_handler(int x, int y) {
-    if (mouse_button == GLUT_LEFT_BUTTON) {
+    if (mouse_button == GLUT_LEFT_BUTTON)
         camera
             .yaw((x - mouse_pos[0]) * 0.1f)
             .pitch((y - mouse_pos[1]) * 0.1f);
-        mouse_pos[0] = x;
-        mouse_pos[1] = y;
-    }
+    mouse_pos[0] = x;
+    mouse_pos[1] = y;
 }
 
 void idle_handler() {
-    for (const std::pair<int, bool>& pr: keymap)
+    for (const auto& pr: keymap)
         if (pr.second) switch (pr.first) {
                 case 'w': camera.move(-camera.target() * camera.speed()); break;
                 case 's': camera.move( camera.target() * camera.speed()); break;
                 case 'd': camera.move(-camera.side() * camera.speed()); break;
                 case 'a': camera.move( camera.side() * camera.speed()); break;
-                case ' ': camera.move( camera.up() * camera.speed()); break;
+                case ' ': camera.move(-camera.up() * camera.speed()); break;
                 case 'c': camera.move( camera.up() * camera.speed()); break;
+                case '-': camera.zoom(-0.1); break;
+                case '+': camera.zoom( 0.1); break;
                 default: break;
         }
     glVertexAttrib3f(shmap["camera_position"],
@@ -215,38 +241,29 @@ void idle_handler() {
 }
 
 void render() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glUseProgram(program_id);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // VP matrix
     linear_algebra::Matrix vp = camera.model();
-    if (vp != prev_camera)
-        cout << "PV:\n" << vp << endl;
-    float buff[16] = {0};
+
 // Grid
-    geometry::grid grid(20);
-    GLuint vao = grid.vertex(shmap["obj_position"]).render().vao();
+    geometry::grid(270)
+            .vertex(shmap["obj_position"])
+            .model(shmap["model"])
+            .mvp(shmap["mvp"])
+            .sampler_selector(shmap["select_samplers"])
+            .material_ambient(shmap["material_ambient"])
+            .material_diffuse(shmap["material_diffuse"])
+            .material_specular(shmap["material_specular"])
+            .material_emission(shmap["material_emission"])
+            .material_shininess(shmap["material_shininess"])
+            .render(vp);
 
-    glBindVertexArray(vao);
-
-    glUniform1i(shmap["select_samplers"], false);
-
-    for (size_t i = 0; i < 4; ++i)
-        for (size_t j = 0; j < 4; ++j)
-            buff[i*4 + j] = vp[i][j];
-    glUniformMatrix4fv(shmap["mvp"], 1, GL_FALSE, buff);
-    for (size_t i = 0; i < 4; ++i)
-        for (size_t j = 0; j < 4; ++j)
-            buff[i*4 + j] = i == j ? 1.f : 0.f;
-    glUniformMatrix4fv(shmap["model"], 1, GL_FALSE, buff);
-
-    glUniform3f(shmap["material_ambient"], 0, .5, .7);
-    glUniform3f(shmap["material_emissive"], .5, .5, .5);
-    glUniform3f(shmap["material_specular"], 1.f, 1.f, 1.f);
-    glUniform1f(shmap["material_shininess"], 3.f);
-    glDrawArrays(GL_LINES, 0, 3 * 2 * 20 * 2);
-    glBindVertexArray(0);
-
-    glutSwapBuffers();
-    glBindTexture(GL_TEXTURE_2D, 0);
     glUseProgram(0);
+    glutSwapBuffers();
+}
+
+int main(int argc, char* argv[]) {
+    renderer(argc, argv);
+    return 0;
 }
