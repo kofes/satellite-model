206ff245d48b413873db47b8372c24ae538559dc
kofes <turnever@gmail.com>
2018-02-14 02:07:55 +1000

    add square class realization

--- geometric_object/square.h
+++ geometric_object/square.h
@@ -1,5 +1,26 @@
 #pragma once
 
-class square {
+#include <cstdlib>
+#include <memory>
 
-};
+#include "geometric_object.h"
+#include "LinearAlgebra.h"
+
+namespace geometry {
+    class quadrilateral : public object {
+    public:
+        quadrilateral(
+                const linear_algebra::Vector& A,
+                const linear_algebra::Vector& B,
+                const linear_algebra::Vector& C,
+                const linear_algebra::Vector& D
+        );
+
+        quadrilateral& render(const linear_algebra::Matrix& vp) override;
+
+    private:
+        class core;
+
+        std::shared_ptr<core> m_core;
+    };
+}
