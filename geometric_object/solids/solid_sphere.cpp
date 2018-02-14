2e4dfc1914a931b2401a1bfd6f379e403105a981
kofes <turnever@gmail.com>
2018-02-14 12:59:13 +1000

    add scale/translate/rotate/color methods to solid::sphere

--- geometric_object/solids/solid_sphere.cpp
+++ geometric_object/solids/solid_sphere.cpp
@@ -7,24 +7,28 @@ class sphere::core {
 public:
     core() = default;
 
-    std::vector<float> positions;
+    std::vector<float> vertices;
     std::vector<float> normals;
     std::vector<float> texture;
     std::vector<GLuint> indices;
 
-    size_t slices;
-    size_t stacks;
     float radius;
 
-    linear_algebra::Vector position = {0.5,0.25,0.5,1};
+    linear_algebra::Vector position = {0, 0, 0, 1};
+    linear_algebra::Vector scale = {1, 1, 1, 1};
+    linear_algebra::Matrix rotate = linear_algebra::Matrix::eye(4, 1);
+
+    float diffuse[3] = {0.5, 0.5, 0.5};
+    float specular[3] = {1.f, 1.f, 1.f};
+    float ambient[3] = {1, 1, 1};
+    float emission[3] = {.5, .5, .5};
+    float shininess = 3.f;
 };
 
 sphere::sphere(float radius, size_t slices, size_t stacks): m_core(new sphere::core) {
-    m_core->slices = slices;
-    m_core->stacks = stacks;
     m_core->radius = radius;
 
-    std::vector<float>& positions = m_core->positions;
+    std::vector<float>& vertices = m_core->vertices;
     std::vector<float>& normals = m_core->normals;
     std::vector<float>& texture = m_core->texture;
 
@@ -39,9 +43,9 @@ sphere::sphere(float radius, size_t slices, size_t stacks): m_core(new sphere::c
             float Z = std::sin(phi) * std::sin(theta);
             float Y = std::cos(phi);
 
-            positions.push_back(X * radius);
-            positions.push_back(Y * radius);
-            positions.push_back(Z * radius);
+            vertices.push_back(X * radius);
+            vertices.push_back(Y * radius);
+            vertices.push_back(Z * radius);
 
             normals.push_back(X);
             normals.push_back(Y);
@@ -65,8 +69,35 @@ sphere::sphere(float radius, size_t slices, size_t stacks): m_core(new sphere::c
     }
 }
 
+const linear_algebra::Vector& sphere::position() const {
+    return m_core->position;
+}
+
+sphere& sphere::scale(const linear_algebra::Vector& scale) {
+    m_core->scale = mvp::action::scale(scale) * m_core->scale;
+    return *this;
+}
+sphere& sphere::translate(const linear_algebra::Vector& move) {
+    m_core->position = mvp::action::translate(move) * m_core->position;
+    return *this;
+}
+sphere& sphere::rotate(const linear_algebra::Vector& axis, double degree) {
+    m_core->rotate = mvp::action::rotate(axis, degree) * m_core->rotate;
+    return *this;
+}
+
+sphere& sphere::color(uint8_t r, uint8_t g, uint8_t b) {
+    float color[] = {r / 255.f, g / 255.f, b / 255.f};
+    for (unsigned char i = 0; i < 3; ++i) {
+        m_core->diffuse[i] *= color[i];
+        m_core->specular[i] *= color[i];
+        m_core->ambient[i] *= color[i];
+        m_core->emission[i] *= color[i];
+    }
+}
+
 sphere &sphere::render(const linear_algebra::Matrix &vp) {
-    const std::vector<float> &positions = m_core->positions;
+    const std::vector<float> &vertices = m_core->vertices;
     const std::vector<float> &normals = m_core->normals;
     const std::vector<float> &texture = m_core->texture;
 
@@ -79,7 +110,7 @@ sphere &sphere::render(const linear_algebra::Matrix &vp) {
     glDeleteBuffers(1, &vbo["vertex"]);
     glGenBuffers(1, &vbo["vertex"]);
     glBindBuffer(GL_ARRAY_BUFFER, vbo["vertex"]);
-    glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(float), positions.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
     glVertexAttribPointer(attr["vertex"], 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     glEnableVertexAttribArray(attr["vertex"]);
 
@@ -95,8 +126,14 @@ sphere &sphere::render(const linear_algebra::Matrix &vp) {
         glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(float), normals.data(), GL_STATIC_DRAW);
         glVertexAttribPointer(attr["normal"], 3, GL_FLOAT, GL_TRUE, 0, nullptr);
         glEnableVertexAttribArray(attr["normal"]);
-        glUniform3f(attr["material_diffuse"], 0.5, 0.5, 0.5);
-        glUniform3f(attr["material_specular"], 1.f, 1.f, 1.f);
+        glUniform3f(attr["material_diffuse"],
+                    m_core->diffuse[0],
+                    m_core->diffuse[1],
+                    m_core->diffuse[2]);
+        glUniform3f(attr["material_specular"],
+                    m_core->specular[0],
+                    m_core->specular[1],
+                    m_core->specular[2]);
     }
 
     if (attr["texture"] != GL_MAX_VERTEX_ATTRIBS) {
@@ -111,12 +148,14 @@ sphere &sphere::render(const linear_algebra::Matrix &vp) {
         glUniform1i(attr["sampler_selector"], false);
 
  	linear_algebra::Matrix model =
-            mvp::action::scale({m_core->radius, m_core->radius, m_core->radius}) *
+            m_core->rotate *
+            mvp::action::scale(m_core->scale) *
             mvp::action::translate(m_core->position).T();
+
     linear_algebra::Matrix mvp = model * vp;
 
-    std::cout << model << std::endl;
-    std::cout << mvp << std::endl;
+    std::cout << "MVP:\n" << mvp << '\n'
+              << "Model:\n" << model << std::endl;
 
     float flat_matrix[16] = {0};
 
@@ -130,9 +169,15 @@ sphere &sphere::render(const linear_algebra::Matrix &vp) {
             flat_matrix[i*4 + j] = model[i][j];
  	glUniformMatrix4fv(attr["model"], 1, GL_FALSE, flat_matrix);
 
-    glUniform3f(attr["material_ambient"], 1, 1, 1);
-    glUniform3f(attr["material_emission"], .5, .5, .5);
-    glUniform1f(attr["material_shininess"], 3.f);
+    glUniform3f(attr["material_ambient"],
+                m_core->ambient[0],
+                m_core->ambient[1],
+                m_core->ambient[2]);
+    glUniform3f(attr["material_emission"],
+                m_core->emission[0],
+                m_core->emission[1],
+                m_core->emission[2]);
+    glUniform1f(attr["material_shininess"], m_core->shininess);
 
     glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
 //    glBindTexture(GL_TEXTURE_2D, 0);
--- geometric_object/solids/solid_sphere.h
+++ geometric_object/solids/solid_sphere.h
@@ -12,6 +12,14 @@ class sphere: public object {
 public:
     sphere(float radius, size_t slices, size_t stacks);
 
+    const linear_algebra::Vector& position() const;
+
+    sphere& scale(const linear_algebra::Vector& scale);
+    sphere& translate(const linear_algebra::Vector& move);
+    sphere& rotate(const linear_algebra::Vector& axis, double degree);
+
+    sphere& color(uint8_t r, uint8_t g, uint8_t b);
+
     sphere& render(const linear_algebra::Matrix& vp) override;
 
 private:
