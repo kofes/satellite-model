6e418ba90fc10e8023e96dedbd5deb018ac822f3
kofes <turnever@gmail.com>
2018-02-20 15:47:35 +1000

    add operator[] - method to color helper

--- helpers/helper_color.h
+++ helpers/helper_color.h
@@ -7,6 +7,24 @@ namespace helper {
 struct color {
     color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
 
+    inline uint8_t& operator[](uint8_t index) {
+        switch (index) {
+            case 0: return r;
+            case 1: return g;
+            case 2: return b;
+            default: throw(std::runtime_error("[helper:color] error: index > 2"));
+        }
+    }
+
+    inline uint8_t operator[](uint8_t index) const {
+        switch (index) {
+            case 0: return r;
+            case 1: return g;
+            case 2: return b;
+            default: throw(std::runtime_error("[helper:color] error: index > 2"));
+        }
+    }
+
     inline std::tuple<double, double, double> d() const {
         return std::make_tuple(r / 255., g / 255., b / 255.);
     };
