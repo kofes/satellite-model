4e72330a3a7a6217f5cc2f8f6d1fc58e686d20aa
kofes <turnever@gmail.com>
2018-02-20 13:35:05 +1000

    add const qualifier to helper::color methods

--- helpers/helper_color.h
+++ helpers/helper_color.h
@@ -7,11 +7,11 @@ namespace helper {
 struct color {
     color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
 
-    inline std::tuple<double, double, double> d() {
+    inline std::tuple<double, double, double> d() const {
         return std::make_tuple(r / 255., g / 255., b / 255.);
     };
 
-    inline std::tuple<float, float, float> f() {
+    inline std::tuple<float, float, float> f() const {
         return std::make_tuple(r / 255.f, g / 255.f, b / 255.f);
     };
 
