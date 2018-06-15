bf072395f7277a597315225fef4f530b27441dfe
kofes <turnever@gmail.com>
2018-06-15 15:42:48 +1000

    add linear-function test for GA

--- src/test/src/helpers/TEST_optimization.h
+++ src/test/src/helpers/TEST_optimization.h
@@ -266,12 +266,13 @@ TEST(OptimizationTest, RosenbrockFunctionNelderMead) {
         return result;
     };
 
-    size_t COUNT_ARGS = 3;
-    double DELTA_H = 0.01;
-
+    size_t COUNT_ITERATIONS = 500;
     size_t error_counter = 0;
 
-    for (size_t i = 0; i < 500; ++i) {
+    for (size_t i = 0; i < COUNT_ITERATIONS; ++i) {
+        size_t COUNT_ARGS = std::rand() % 10 + 1;
+        double DELTA_H = 0.01;
+
         auto vec = helper::optimization::amoeba(fun,
             false,
             COUNT_ARGS,
@@ -285,32 +286,49 @@ TEST(OptimizationTest, RosenbrockFunctionNelderMead) {
                 break;
             }
     }
-    std::cout << "COUNT ERRORS: " << error_counter << std::endl;
-    //
-    // for (size_t i = 0; i < vec.size(); ++i)
-    //     EXPECT_LE(std::fabs(vec[i] - 1), DELTA_H / 2);
+
+    EXPECT_LE(error_counter * 1. / COUNT_ITERATIONS, 0.05);
 }
 
 
-// // TEST: Genetic algorithm optimization for linear function
-// TEST(OptimizationTest, LinearFunctionGenetic) {
-//     auto fun = [&] (const linear_algebra::Vector& vec) {
-//         double result = 0;
-//         for (size_t i = 0; i < vec.size(); ++i)
-//             result += vec[i];
-//         return result;
-//     };
-//
-//     size_t COUNT_ARGS = 6;
-//     double DELTA_H = 1e-5;
-//
-//     auto vec = helper::optimization::genetic(fun,
-//         false,
-//         COUNT_ARGS,
-//         -10, 10,
-//         DELTA_H
-//     );
-//
-//     for (size_t i = 0; i < vec.size(); ++i)
-//         EXPECT_LE(std::fabs(vec[i]), DELTA_H / 2);
-// }
+// TEST: Genetic algorithm optimization for linear function
+TEST(OptimizationTest, LinearFunctionGenetic) {
+    auto fun = [&] (const linear_algebra::Vector& vec) {
+        double result = 0;
+        for (size_t i = 0; i < vec.size(); ++i)
+            result += vec[i];
+        return result;
+    };
+
+    size_t COUNT_ITERATIONS = 500;
+    size_t error_counter = 0;
+
+    size_t COUNT_ARGS = 6;
+    double DELTA_H = 1e-3;
+
+    for (size_t i = 0; i < COUNT_ITERATIONS; ++i) {
+        size_t COUNT_ARGS = std::rand() % 10 + 1;
+        double DELTA_H = 0.1;
+
+        auto vec = helper::optimization::genetic(fun,
+            false,
+            COUNT_ARGS,
+            linear_algebra::Vector(COUNT_ARGS, -10),
+            linear_algebra::Vector(COUNT_ARGS, 10),
+            100 + 0.5 / DELTA_H, // count iterations
+            150 + 0.1 / DELTA_H, // initial population size
+            100 + 0.3 / DELTA_H, // common population size
+            200 + 0.2 / DELTA_H, // count crossovers
+            35 + 0.1 / DELTA_H, // count mutations
+            0.5 // scale factor
+        );
+
+        for (size_t i = 0; i < vec.size(); ++i)
+            if (std::fabs(vec[i]+10) > DELTA_H * std::sqrt(COUNT_ARGS)) {
+                ++error_counter;
+                break;
+            }
+    }
+
+    EXPECT_LE(error_counter * 1. / COUNT_ITERATIONS, 0.1);
+}
